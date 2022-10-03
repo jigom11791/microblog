efbcf7938bda53c4ea75100b11ef52e5b69e6cad
jigom11791 <jigom11791@gmail.com>
2022-10-03 13:29:49 -0700

    init commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
+# https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iii-web-forms
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 from app import routes
